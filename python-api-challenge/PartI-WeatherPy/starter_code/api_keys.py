9587d84c117b3aa10987cdf9c4beb99df7212c04
Lina Sachuk <sachuklina@gmail.com>
2020-01-23 02:44:51 -0700

    part 1 : added row data to csv file and created Data Frame

--- python-api-challenge/PartI-WeatherPy/starter_code/api_keys.py
+++ python-api-challenge/PartI-WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ad6d5907b7844997f0cc1d72091d8766"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB9Slst_TQxNixIRYxvw8bhCj8Y6ApCcSE"
