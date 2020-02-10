6bb27479e4c0f787e64dd7a7f9e9be44b8bbd024
Lina Sachuk <sachuklina@gmail.com>
2020-02-10 16:28:54 -0700

    added first 4 parts

--- sql-challenge/EmployeeSQL/HomeWork.sql
+++ sql-challenge/EmployeeSQL/HomeWork.sql
@@ -23,7 +23,12 @@ ON e.emp_no = dm.emp_no;
 
 -- 4. List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
-
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name 
+FROM dept_emp de
+LEFT JOIN employees e
+ON e.emp_no = de.emp_no
+LEFT JOIN departments d
+ON d.dept_no = de.dept_no
 
 
 
