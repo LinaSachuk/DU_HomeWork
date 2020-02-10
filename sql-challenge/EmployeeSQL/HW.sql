bd33f5365a164c66aa3e68571a799498124157b0
Lina Sachuk <sachuklina@gmail.com>
2020-02-10 16:21:03 -0700

    added first 3 parts

---
+++ sql-challenge/EmployeeSQL/HW.sql
@@ -0,0 +1,35 @@
+-- 1. List the following details of each employee: 
+-- employee number, last name, first name, gender, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e
+INNER JOIN salaries s
+ON s.emp_no = e.emp_no;
+
+-- 2. List employees who were hired in 1986.
+SELECT * 
+FROM employees
+WHERE date_part('year', hire_date)= 1986;
+
+-- 3. List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, 
+-- last name, first name, and start and end employment dates.
+SELECT  dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
+FROM dept_manager dm
+LEFT JOIN departments d
+ON dm.dept_no = d.dept_no
+LEFT JOIN employees e
+ON e.emp_no = dm.emp_no;	
+
+-- 4. List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
+
+
+
+
+	
+	
+	
+	
+	
+	
