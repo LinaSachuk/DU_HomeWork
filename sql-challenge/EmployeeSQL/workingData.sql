c8ca11394fe042f4c332b721bba48795aeaf8d89
Lina Sachuk <sachuklina@gmail.com>
2020-02-09 23:08:24 -0700

    added workingData file

---
+++ sql-challenge/EmployeeSQL/workingData.sql
@@ -0,0 +1,15 @@
+DROP TABLE departments, dept_emp, dept_manager, employees, salaries, titles;
+
+select * from departments;
+-- delete from departments
+-- where dept_no = 'dept_no'
+
+select * from dept_emp;
+
+select * from dept_manager;
+
+select * from employees;
+
+select * from salaries;
+
+select * from titles;
