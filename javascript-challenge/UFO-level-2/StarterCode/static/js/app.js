792cab18ff74c64b34bd5bd1cb5253f598294354
Lina Sachuk <sachuklina@gmail.com>
2020-03-23 00:54:21 -0600

    added full bonus

--- javascript-challenge/UFO-level-2/StarterCode/static/js/app.js
+++ javascript-challenge/UFO-level-2/StarterCode/static/js/app.js
@@ -4,7 +4,12 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var dates = data.map(item => item.datetime);
-// console.log(dates.length);
+var cities = data.map(item => item.city);
+var states = data.map(item => item.state);
+var countries = data.map(item => item.country);
+var shape = data.map(item => item.shape);
+
+console.log(dates);
 //  we have 111 items in the array
 
 // console.log(`from ${dates[0]} to ${dates[110]}`);
@@ -17,43 +22,43 @@ var button = d3.select('#filter-btn');
 
 //filter parameter
 var search = d3.selectAll('.search');
-console.log(search);
+// console.log(search);
 
-
+var searchParam = '';
 
 search.on("click", function() {
 
-    var searchParam = d3.select(this).text()
+    searchParam = d3.select(this).text()
 
     console.log(searchParam);
     document.getElementById("datetime").value = "";
     document.getElementById("datetime").placeholder = searchParam;
     d3.select("#searchText").text(`Enter a ${searchParam}`);
-
+    return searchParam;
 });
 
 
-
-
 button.on("click", function() {
-
+    // console.log(`test ${searchParam}`)
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
-
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-
+    inputValue = inputValue.toLowerCase();
     console.log(inputValue);
+    // console.log(`me ${dates.indexOf(inputValue)}`);
 
 
-    if (dates.indexOf(inputValue) === -1) {
+    // ||cities.indexOf(inputValue) === -1 || states.indexOf(inputValue) === -1 || countries.indexOf(inputValue) === -1 || shape.indexOf(inputValue) === -1
+
+    if (dates.indexOf(inputValue) === -1 && cities.indexOf(inputValue) === -1 && states.indexOf(inputValue) === -1 && countries.indexOf(inputValue) === -1 && shape.indexOf(inputValue) === -1) {
         // push here because a value of -1 means it's not in the array
         console.log('I am not here!')
         document.getElementById("datetime").value = "";
-        document.getElementById("datetime").placeholder = "Try another date ... from 1/1/2010 to 1/13/2010 ";
+        document.getElementById("datetime").placeholder = "Try again. ";
     };
 
-    var filteredData = tableData.filter(item => item.datetime === inputValue);
+    var filteredData = tableData.filter(item => item[searchParam] === inputValue);
 
     console.log(filteredData);
 
@@ -80,8 +85,8 @@ button.on("click", function() {
         var cells = rows.selectAll('td');
         cells.remove();
         var row = tbody.append("tr");
-        var cell = row.append("td").style('color', "red");
-        cell.text('No Data Found!');
+        var cell = row.append("td").style('color', "orange");
+        cell.text(`No Data Found for this ${searchParam}!`).style("font-size", "24px");
     }
 
 });
