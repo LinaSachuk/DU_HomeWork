8d4f9394e25ecfd055e3ee8d7de4d10fadaa9dac
Lina Sachuk <sachuklina@gmail.com>
2020-04-09 03:26:27 -0600

    added first part of hw

--- leaflet-challenge/Leaflet-Step-1/static/js/logic.js
+++ leaflet-challenge/Leaflet-Step-1/static/js/logic.js
@@ -9,23 +9,49 @@ d3.json(queryUrl, function (data) {
     console.log('data.features:', data.features)
 });
 
+//color based on value
+function getColor(d) {
+    return d > 6 ? '#EE162D' :
+        d > 5 ? '#F35B6C' :
+            d > 4 ? '#F8A4AD' :
+                d > 3 ? '#f3985b' :
+                    d > 2 ? '#f3d65b' :
+                        d > 1 ? '#cbf35b' :
+                            '#6cf35b';
+}
+
+
 function createFeatures(earthquakeData) {
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place, time and mag of the earthquake
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(
-            '<h3>' +
-            feature.properties.place +
-            '</h3><hr><p>' +
-            new Date(feature.properties.time) +
-            '</p><hr><p>' + feature.properties.mag + 'mag </p>'
-        );
-    }
+
+    function myStyle(f) {
+        return {
+            radius: f.properties.mag * 6,
+            color: "black",
+            fillColor: getColor(f.properties.mag),
+            fillOpacity: 0.6,
+            weight: 0.5,
+            opacity: 1
+        }
+    };
+
 
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+
+            return L.circleMarker(latlng, myStyle(feature));
+        },
+        // Define a function we want to run once for each feature in the features array
+        // Give each feature a popup describing the place, time and mag of the earthquake
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                '<h3>' +
+                feature.properties.place +
+                '</h3><hr><p>' +
+                new Date(feature.properties.time) +
+                '</p><hr><p>Magnitude: ' + feature.properties.mag + '</p>');
+        }
     });
     console.log('earthquakes:', earthquakes);
 
@@ -68,12 +94,36 @@ function createMap(earthquakes) {
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
-    L.control
-        .layers(baseMaps, overlayMaps, {
-            collapsed: false,
-        })
-        .addTo(myMap);
-}
+    // L.control
+    //     .layers(baseMaps, overlayMaps, {
+    //         collapsed: false,
+    //     })
+    //     .addTo(myMap);
 
+
+
+
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 1, 2, 3, 4, 5],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+
+
+}
 
 
